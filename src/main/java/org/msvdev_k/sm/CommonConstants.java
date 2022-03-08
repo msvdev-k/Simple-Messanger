73f7b6a19dc14f4d4e45e60e18550ea70fc8f350
msvdev-k <msvdev.k@gmail.com>
2022-03-08 19:03:43 +0300

    server

---
+++ src/main/java/org/msvdev_k/sm/CommonConstants.java
@@ -0,0 +1,19 @@
+package org.msvdev_k.sm;
+
+
+/**
+ * Класс, описывающий основные константы.
+ */
+public class CommonConstants {
+
+    /**
+     * Сетевой адрес сервера.
+     */
+    public static final String SERVER_ADDRESS = "localhost";
+
+    /**
+     * Порт, прослушиваемый сервером.
+     */
+    public static final int SERVER_PORT = 8189;
+
+}
